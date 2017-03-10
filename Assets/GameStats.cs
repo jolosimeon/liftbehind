6c2cbd12d19c2391e5a8310f4d9b985b2f1ffa33
Yolo Jolo <jolosimeon@users.noreply.github.com>
2017-03-11 05:40:46 +0800

    sound fx main screen, some scripts

---
+++ Assets/GameStats.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameStats {
+
+    private static int survToSave = 15;
+    private static int survSaved = 0;
+    private static int floors = 20;
+    private static int currFloor = 0;
+
+    public static int SurvToSave
+    {
+        get
+        {
+            return survToSave;
+        }
+        set
+        {
+            survToSave = value;
+        }
+    }
+
+    public static int SurvSaved
+    {
+        get
+        {
+            return survSaved;
+        }
+        set
+        {
+            survSaved = value;
+        }
+    }
+
+    public static int Floors
+    {
+        get
+        {
+            return floors;
+        }
+        set
+        {
+            floors = value;
+        }
+    }
+
+    public static int CurrFloor
+    {
+        get
+        {
+            return currFloor;
+        }
+        set
+        {
+            currFloor = value;
+        }
+    }
+
+
+}
