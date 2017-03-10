798c2dfc4afde831275f575a67552155f70dc1e1
Jan Tristan Milan <jtbmilan@gmail.com>
2017-03-11 03:01:34 +0800

    Implement flashlight toggle and remove ambient lighting

---
+++ Assets/Scripts/FlashlightToggle.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+private var lightSource : Light;
+var soundTurnOn : AudioClip;
+var soundTurnOff : AudioClip;
+  
+function Start () {
+     lightSource = GetComponent(Light);
+}
+  
+function Update () {
+	if (Input.GetMouseButtonDown(1)) ToggleFlashLight();
+}
+  
+function ToggleFlashLight () {
+	lightSource.enabled=!lightSource.enabled;
+
+	//Audio
+	if (lightSource.enabled) {
+		GetComponent.<AudioSource>().clip = soundTurnOn;
+	} else {
+		GetComponent.<AudioSource>().clip = soundTurnOff;
+    }
+    GetComponent.<AudioSource>().Play();
+ }
+  
+ @script RequireComponent(AudioSource)
