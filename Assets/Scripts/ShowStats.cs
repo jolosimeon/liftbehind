7e2b47db8fa12e2ec3dca19d4a3b42ba16916563
Jan Tristan Milan <jtbmilan@gmail.com>
2017-03-11 23:40:38 +0800

    Add stats for number of survivors saved

---
+++ Assets/Scripts/ShowStats.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowStats : MonoBehaviour {
+	int numOfSurvivorsSaved;
+
+	// Use this for initialization
+	void Start () {
+		numOfSurvivorsSaved = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Semicolon)) {
+			numOfSurvivorsSaved++;
+		}
+	}
+
+	void OnGUI() {
+		GUI.skin.textArea.fontSize = 18;
+		GUI.skin.textArea.normal.background = null;
+		GUI.skin.textArea.active.background = null;
+		GUI.TextArea (new Rect (10,10, 300, 100), "Number of survivors saved: " + numOfSurvivorsSaved);
+	}
+}
