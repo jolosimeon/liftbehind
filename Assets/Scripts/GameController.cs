7036c5b33e1134ceca569a3be309ce32a77b6000
Yolo Jolo <jolosimeon@users.noreply.github.com>
2017-03-11 21:54:58 +0800

    Random spawning

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if wala na hp
+		
+        //aakyat
+        if (GameStats.GoingUp) {
+            GameStats.CurrFloor++;
+
+            if (GameStats.CurrFloor == GameStats.Floors) {
+                //game over na pag wala na floors
+                //nasa taas ka na
+            }
+
+            else {
+
+                //clean hallway
+                GameObject[] gameObjectArray = GameObject.FindGameObjectsWithTag("Enemy");
+                foreach (GameObject go in gameObjectArray) {
+                    go.SetActive(false);
+                }
+
+                gameObjectArray = GameObject.FindGameObjectsWithTag("Survivor");
+                foreach (GameObject go in gameObjectArray)
+                {
+                    go.SetActive(false);
+                }
+
+                System.Random rand = new System.Random();
+                int nextLevel = rand.Next(0, 3);
+
+                //spawn stuff
+                switch (nextLevel)
+                {
+                    case 0: //spawn nothing
+                        break;
+                    case 1: //spawn zombeh
+                        gameObjectArray = GameObject.FindGameObjectsWithTag("Enemy");
+                        foreach (GameObject go in gameObjectArray)
+                        {
+                            go.SetActive(false);
+                        }
+                        break;
+                    case 2: //spawn hooman
+                        gameObjectArray = GameObject.FindGameObjectsWithTag("Survivor");
+                        foreach (GameObject go in gameObjectArray)
+                        {
+                            go.SetActive(false);
+                        }
+                        break;
+                    case 3: //spawn zommbeh and hooman
+                        gameObjectArray = GameObject.FindGameObjectsWithTag("Enemy");
+                        foreach (GameObject go in gameObjectArray)
+                        {
+                            go.SetActive(false);
+                        }
+                        break;
+                        gameObjectArray = GameObject.FindGameObjectsWithTag("Survivor");
+                        foreach (GameObject go in gameObjectArray)
+                        {
+                            go.SetActive(false);
+                        }
+                        break;
+                    default: //lets party
+                        break;
+                }
+
+            }
+        }
+	}
+}
--- Assets/Scripts/GameStats.cs
+++ Assets/Scripts/GameStats.cs
@@ -7,7 +7,11 @@ public static class GameStats {
     private static int survToSave = 15;
     private static int survSaved = 0;
     private static int floors = 20;
-    private static int currFloor = 0;
+    private static int currFloor = 1;
+    private static int score = 0;
+    private static bool goingUp = false;
+    private static int hp = 100;
+
 
     public static int SurvToSave
     {
@@ -54,6 +58,18 @@ public static class GameStats {
         set
         {
             currFloor = value;
+        }
+    }
+
+    public static bool GoingUp
+    {
+        get
+        {
+            return goingUp;
+        }
+        set
+        {
+            goingUp = value;
         }
     }
 
