53eff484e5ed249755184414cc351ab36be324c8
Jan Tristan Milan <jtbmilan@gmail.com>
2017-04-11 05:54:40 +0800

    Rename object highlighter

--- Assets/Scripts/MouseHighlight.cs
+++ Assets/Scripts/MouseHighlight.cs
@@ -2,26 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectHighlighter : MonoBehaviour {
+public class MouseHighlight : MonoBehaviour {
 
-	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		
 	}
 
-	void OnMouseEnter()
-	{
-		Debug.Log ("AHAW");
+	void OnMouseEnter() {
 		gameObject.GetComponent<Renderer>().material.shader = Shader.Find("Custom/ImageEffectShader");
 	}
-	void OnMouseExit()
-	{
+
+	void OnMouseExit() {
 		gameObject.GetComponent<Renderer>().material.shader = Shader.Find("Diffuse");
 	}
-
 }
