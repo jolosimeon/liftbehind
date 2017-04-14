6a1ceec19f1e5c461426e31e998c9d8e2c517ce5
Jan Tristan Milan <jtbmilan@gmail.com>
2017-04-14 09:38:50 +0800

    Add difficulty selection menu design

---
+++ Assets/We Are Studios/Scripts/Game Menu/GameMenuManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenuManager : MonoBehaviour {
+
+
+	private void Start () {
+		
+	}
+
+	private void Update () {
+		
+	}
+
+	public void StartGame() {
+		
+	}
+
+	public void Instructions() {
+
+	}
+}
