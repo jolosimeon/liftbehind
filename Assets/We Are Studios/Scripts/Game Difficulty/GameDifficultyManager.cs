95a5b57f5b8309f2b9f771d0b1940bc06538fef0
Jan Tristan Milan <jtbmilan@gmail.com>
2017-04-14 10:31:23 +0800

    Implement loading of gamplay from difficulty screen

---
+++ Assets/We Are Studios/Scripts/Game Difficulty/GameDifficultyManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameDifficultyManager : MonoBehaviour {
+	private const float EASY_ZOMBIE_SPEED = 1.0f;
+	private const int EASY_NUMBER_PRESSES_OPEN = 10;
+	private const int EASY_NUMBER_PRESSES_CLOSE = 10;
+
+	private const float MEDIUM_ZOMBIE_SPEED = 2.0f;
+	private const int MEDIUM_NUMBER_PRESSES_OPEN = 50;
+	private const int MEDIUM_NUMBER_PRESSES_CLOSE = 50;
+
+	private const float HARD_ZOMBIE_SPEED = 3.0f;
+	private const int HARD_NUMBER_PRESSES_OPEN = 90;
+	private const int HARD_NUMBER_PRESSES_CLOSE = 90;
+
+	private float zombieSpeed;
+	private int numPressesOpen;
+	private int numPressesClose;
+
+
+	public float GetZombieSpeed() {
+		return zombieSpeed;
+	}
+
+	public int GetNumberOfPressesOpen() {
+		return numPressesOpen;
+	}
+
+	public int GetNumberOfPressesClose() {
+		return numPressesClose;
+	}
+		
+	public void SelectEasy() {
+		zombieSpeed = EASY_ZOMBIE_SPEED;
+		numPressesOpen = EASY_NUMBER_PRESSES_OPEN;
+		numPressesClose = EASY_NUMBER_PRESSES_CLOSE;
+		SceneManager.LoadScene ("Gameplay");
+	}
+
+	public void SelectMedium() {
+		zombieSpeed = MEDIUM_ZOMBIE_SPEED;
+		numPressesOpen = MEDIUM_NUMBER_PRESSES_OPEN;
+		numPressesClose = MEDIUM_NUMBER_PRESSES_CLOSE;
+		SceneManager.LoadScene ("Gameplay");
+	}
+
+	public void SelectHard() {
+		zombieSpeed = HARD_ZOMBIE_SPEED;
+		numPressesOpen = HARD_NUMBER_PRESSES_OPEN;
+		numPressesClose = HARD_NUMBER_PRESSES_CLOSE;
+		SceneManager.LoadScene ("Gameplay");
+	}
+
+	private void Start () {
+		
+	}
+	
+
+	private void Update () {
+		
+	}
+
+	private void Awake() {
+		DontDestroyOnLoad (transform.gameObject);
+	}
+}
--- Assets/We Are Studios/Scripts/Game Menu/GameMenuManager.cs
+++ Assets/We Are Studios/Scripts/Game Menu/GameMenuManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameMenuManager : MonoBehaviour {
 
@@ -14,7 +15,7 @@ public class GameMenuManager : MonoBehaviour {
 	}
 
 	public void StartGame() {
-		
+		SceneManager.LoadScene ("Game Difficulty");
 	}
 
 	public void Instructions() {
