0dc5aa846b453073e39ccc87e13cbf7dfb34a38e
John <johncaingles@gmail.com>
2017-04-16 01:28:19 +0800

    added flickering functionality to flashlight

--- Assets/Menu/Flicker.js
+++ Assets/Menu/Flicker.js
@@ -3,16 +3,54 @@
 var FlashingLight : Light;
 FlashingLight.enabled = false;
 
+public var randomFrequency : float; // the lower the less naka-on light
+public var flickerTimeInterval : float; // every ilang secs mag fflicker
+public var flickerTimeDuration : float;
+
+//var shouldFlicker = false;
+private var nextFlickerTime : float;
+private var flickerEndTime : float;
+var isFlickering;
+
+
+function Start() {
+	nextFlickerTime = Time.time + flickerTimeInterval;
+	isFlickering = false;
+}
 
 function FixedUpdate () {
 
-var RandomNumber = Random.value;
+	var RandomNumber = Random.value;
 
-
-    if(RandomNumber<=.9){
-    FlashingLight.enabled = true;
-    
+	if (checkIfShouldFlicker()){
+		// originally 0.9
+	    if(RandomNumber<=randomFrequency){
+	    FlashingLight.enabled = true;
+	    
+	    }
+	    else
+	    FlashingLight.enabled=false;
     }
-    else
-    FlashingLight.enabled=false;
+    else FlashingLight.enabled=true;
+
+}
+
+function checkIfShouldFlicker(){
+	if(isFlickering){
+		if(Time.time >= flickerEndTime){
+			nextFlickerTime = Time.time + flickerTimeInterval;
+			isFlickering = false;
+			return false;
+		}
+		else return true;
+	}
+	else {
+		if(Time.time >= nextFlickerTime){ // starts here
+			flickerEndTime = Time.time + flickerTimeDuration;
+			isFlickering = true;
+			return true;
+		}
+		else return false;
+	}
+
 }
