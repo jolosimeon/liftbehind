c4a3349d3c97a98d3d48e04d471275076aa3763e
Jaylica <jaylicaannetan@yahoo.com>
2017-04-16 18:20:42 +0800

    Added Button Sounds

---
+++ Assets/We Are Studios/Scripts/ButtonManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour {
+	public AudioSource source;
+	public AudioClip hover;
+	public AudioClip click;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void Onhover()
+	{
+		source.PlayOneShot (hover);
+	}
+
+	public void Onclick()
+	{
+		source.PlayOneShot (click);
+	}
+
+}
